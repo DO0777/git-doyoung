9453f2894aef831dd9693a586e2f5d75770969c5
doyoung <do1007y@naver.com>
2022-09-22 14:49:17 +0900

    buffer

---
+++ src/cch211/MakeReportTest.java
@@ -0,0 +1,13 @@
+package cch211;
+
+public class MakeReportTest {
+
+	public static void main(String[] args) {
+
+	 MakeReport builder = new MakeReport();
+	 String report = builder.gerReport();
+	 
+	 System.out.println(report);
+	}
+
+}
