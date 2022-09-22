669feced4ac4f14907d94aa86e9983092f58af5b
doyoung <do1007y@naver.com>
2022-09-22 15:38:12 +0900

    객체지향 프로그래밍

---
+++ src/cch214/Subway.java
@@ -0,0 +1,23 @@
+package cch214;
+
+public class Subway {
+	
+	int lineNumber;
+	int passengerCount;
+	int money;
+	
+	public Subway(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+	
+	public void take(int money) {
+		this.money += money;
+		passengerCount++;
+	}
+	
+	public void showSubwayInfo() {
+		
+		System.out.println(lineNumber +"번의 승객 수는" + passengerCount + "명이고, 수입은" + money + "원입니다.");
+	}
+
+}
---
+++ src/cch214/TakeTransTest.java
@@ -0,0 +1,28 @@
+package cch214;
+
+public class TakeTransTest {
+
+	public static void main(String[] args) {
+
+		Student studentJ = new Student("James", 5000);
+		Student studentT = new Student("Tomas", 10000);
+		
+		Bus bus100 = new Bus(100);
+		Bus bus500 = new Bus(500);
+		
+		studentJ.takeBus(bus100);
+		
+		Subway geeenSubway = new Subway(2);
+		studentT.takeSubway(geeenSubway);
+		
+		studentJ.showInfo();
+		studentT.showInfo();
+		
+		bus100.showBusInfo();
+		geeenSubway.showSubwayInfo();
+		
+		bus500.showBusInfo();
+		
+	}
+
+}
