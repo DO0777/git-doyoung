f55b3d9b9c9b927efb8de6165a2be0ac1a32696e
doyoung <do1007y@naver.com>
2022-09-27 14:38:14 +0900

    3-09

---
+++ src/cch309/ComputerTest.java
@@ -0,0 +1,15 @@
+package cch309;
+
+public class ComputerTest {
+
+	public static void main(String[] args) {
+
+		Computer computer = new DeskTop();
+		computer.display();
+		computer.turnOff();
+		
+		NoteBook myNote = new MyNoteBook();
+		
+	}
+
+}
